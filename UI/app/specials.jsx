80cdc28433a92c145a10060f74f2f799de18965a
CCents <christian.c.kehr@gmail.com>
2025-06-19 14:35:47 -0400

    Added expo foundation into UI folder

---
+++ UI/app/specials.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Specials() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Shane Mundorf is a dad actor...</Text>
+    </View>
+  );
+} 
